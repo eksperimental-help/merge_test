2e3476a557d06ed10b035bc3c74fc460cf9160b2
Eksperimental <eksperimental@autistici.org>
2022-02-17 07:32:07 -0500

    Initial commit

---
+++ lib/merge_test.ex
@@ -0,0 +1,18 @@
+defmodule MergeTest do
+  @moduledoc """
+  Documentation for `MergeTest`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MergeTest.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/merge_test_test.exs
@@ -0,0 +1,8 @@
+defmodule MergeTestTest do
+  use ExUnit.Case
+  doctest MergeTest
+
+  test "greets the world" do
+    assert MergeTest.hello() == :world
+  end
+end
